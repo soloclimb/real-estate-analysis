fd2d543e18c80994c4281b4ed4bb877b2c185c0f
soloclimb <keito.eternal@gmail.com>
2024-01-04 21:52:22 +0200

    Create separate config with resources columns - Create separate json file to store columns - Iterate on it in main

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from scripts.extract.extract import get_world_bank_resource
 from scripts.load.load import insert_records
 
-resource_columns = ['date_disclosed', 'project_url', 
-                    'company_name', 'country', 'industry',
-                    'status', 'ifc_invested_date', 
-                    'ifc_investment_for_risk_management_million_usd', 
-                    'ifc_investment_for_loan_million_usd', 
-                    'total_ifc_investment_as_approved_by_board_million_usd', 
-                    'as_of_date']
+from json import load
+resourses_columns = None
+with open("./config/extract-config.json", "r") as config:
+    resourses_columns = load(config)
 
-result = get_world_bank_resource(resource='efin-cagm', resourse_columns=resource_columns,  format='json', limit=60)
-insert_records(record_name='investment_projects', records_dct= result)
+for resource in resourses_columns:
+    result = get_world_bank_resource(resource='efin-cagm', resourse_columns=resourses_columns[resource],  format='json', limit=60)
+    insert_records(record_name='investment_projects', records_dct= result)
