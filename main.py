54db60d01067609f1d0d6d6fa2418a310d24f07d
soloclimb <keito.eternal@gmail.com>
2024-01-04 20:56:20 +0200

    Extraction of only needed columns - Created a list of columns in main file, query for SodaAPI to retrieve only needed data

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from scripts.extract.extract import get_world_bank_resource
 from scripts.load.load import insert_records
 
-result = get_world_bank_resource(resource='efin-cagm', format='json', limit=60)
-print(result)
+resource_columns = ['date_disclosed', 'project_url', 
+                    'company_name', 'country', 'industry',
+                    'status', 'ifc_invested_date', 
+                    'ifc_investment_for_risk_management_million_usd', 
+                    'ifc_investment_for_loan_million_usd', 
+                    'total_ifc_investment_as_approved_by_board_million_usd', 
+                    'as_of_date']
+
+result = get_world_bank_resource(resource='efin-cagm', resourse_columns=resource_columns,  format='json', limit=60)
 insert_records(record_name='investment_projects', records_dct= result)
--- scripts/extract/extract.py
+++ scripts/extract/extract.py
@@ -1,7 +1,8 @@
 from sodapy import Socrata
 from dotenv import dotenv_values
 
-def get_world_bank_resource(resource, format, limit):
+def get_world_bank_resource(resource, format, limit, resourse_columns):
+    
     domain = dotenv_values(".env")['domain']
     client = Socrata(domain, None)
-    return client.get(resource, content_type=format, limit=limit)
+    return client.get(resource, query=f"SELECT {', '.join(resourse_columns)} LIMIT {limit}", content_type=format)
